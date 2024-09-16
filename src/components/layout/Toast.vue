8333d3abfd114387886d2551fa48f6cb98d2efb9
gsusgit <fernandezjodar.jesus@gmail.com>
2024-09-16 12:35:57 +0200

    Change toast background

--- src/components/layout/Toast.vue
+++ src/components/layout/Toast.vue
@@ -15,12 +15,12 @@ const {
       name="fade">
     <div
         v-if="showToast"
-        :class="[color === 'success' ? 'bg-green-200' : 'bg-red-200']"
+        :class="[color === 'success' ? 'bg-gray-50' : 'bg-red-50']"
         class="toast fixed top-24 right-4 w-full max-w-xs p-4 text-gray-500 rounded-lg shadow"
         role="alert">
       <button
           type="button"
-          :class="[color === 'success' ? 'text-green-950 hover:text-green-800' : 'text-red-950 hover:text-red-800']"
+          :class="[color === 'success' ? 'text-teal-950 hover:text-teal-800' : 'text-red-950 hover:text-red-800']"
           class="ms-auto -mx-1.5 -my-1.5 rounded-lg focus:ring-2 p-1.5 inline-flex items-center justify-center h-8 w-8"
           @click="hide"
           aria-label="Close">
@@ -40,7 +40,7 @@ const {
       </button>
       <span
           class="ml-3"
-          :class="[color === 'success' ? 'text-green-950' : 'text-red-950']"
+          :class="[color === 'success' ? 'text-teal-950' : 'text-red-950']"
       >
         {{message}}
       </span>
